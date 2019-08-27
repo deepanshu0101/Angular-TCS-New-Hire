dd553e313ee1931a5d8f8a0c07321d1d0c05aa14
deepanshu0101 <deepanshu.kachhwaha@simplilearn.net>
2019-08-27 18:07:40 +0530

    26th Aug
    26th Aug

---
+++ 26 August/user-ap/src/app/demoapp.component.ts
@@ -0,0 +1,10 @@
+
+import {Component} from '@angular/core'
+
+@Component({
+    selector:'demo-app',
+    templateUrl:'./demoapp.component.html' 
+})
+export class DemoApp {
+    appname="DemoApp Running"
+}
